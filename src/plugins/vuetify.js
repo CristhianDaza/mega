2edefe6f12c2891ee900ea783a7bd27367b9d609
Cristhian Daza <cristian.d.b@hotmail.com>
2020-08-03 17:38:39 -0500

    Inicializando Proyecto en limpio

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,8 +6,8 @@ import es from 'vuetify/es5/locale/es';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    lang: {
-      locales: { es },
-      current: 'es',
-    },
+  lang: {
+    locales: { es },
+    current: 'es',
+  },
 });
---
+++ src/views/Inicio.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h2>Inicio</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Inicio',
+};
+</script>
