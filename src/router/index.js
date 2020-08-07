5154cb9e722dde899b155c7279a402bf7b189987
Cristhian Daza <cristian.d.b@hotmail.com>
2020-08-07 12:35:30 -0500

    Protegiendo Rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { auth } from '@/firebase';
 
 Vue.use(VueRouter);
 
@@ -63,60 +64,70 @@ const routes = [
     name: 'Admin',
     exact: true,
     component: () => import('../views/admin/Admin.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/agregar-trabajo',
     name: 'Agregar Trabajo',
     exact: true,
     component: () => import('../views/admin/AgregarTrabajo.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/usuarios',
     name: 'Usuarios',
     exact: true,
     component: () => import('../views/admin/usuarios/Index.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/usuarios/agregar-usuario',
     name: 'Agregar Usuarios',
     exact: true,
     component: () => import('../views/admin/usuarios/AgregarUsuario.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/carousel',
     name: 'Carousel',
     exact: true,
     component: () => import('../views/admin/carousel/Index.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/carousel/agregar-carousel',
     name: 'Agregar Carousel',
     exact: true,
     component: () => import('../views/admin/carousel/AgregarCarousel.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/catalogos',
     name: 'Catalogos',
     exact: true,
     component: () => import('../views/admin/catalogos/Index.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/catalogos/agregar-catalogo',
     name: 'Agregar Catalogo',
     exact: true,
     component: () => import('../views/admin/catalogos/AgregarCatalogo.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/imagen-info',
     name: 'Imagen Informartiva',
     exact: true,
     component: () => import('../views/admin/imagenInfo/Index.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin/imagen-info/agregar-imagen-info',
     name: 'Agregar Imagen Informativa',
     exact: true,
     component: () => import('../views/admin/imagenInfo/AgregarImagenInfo.vue'),
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -128,6 +139,20 @@ const router = new VueRouter({
   scrollBehavior(to, from, savedPosition) {
     return { x: 0, y: 0 };
   },
+});
+
+router.beforeEach((to, from, next) => {
+  const user = auth.currentUser;
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (user) {
+      next();
+    } else {
+      next({ name: 'Login' });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
