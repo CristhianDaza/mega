4cb7063842e42ad892cd131f3cdd1ecda3d5e1a1
Cristhian Daza <cristian.d.b@hotmail.com>
2020-08-14 12:24:03 -0500

    Mostrando tabla de trabajos

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="this.$vuetify.breakpoint.xs ? '' : 'container'">
+  <v-container fluid>
     <v-btn
       color="success"
       large
@@ -10,182 +10,192 @@
     </v-btn>
     <v-divider></v-divider>
 
-    <v-row class="fill-height mx-0">
-      <v-col cols="12">
-        <v-sheet height="64">
-          <v-toolbar flat color="blue-grey darken-4">
-
-            <v-btn outlined class="mr-4" @click="setToday">
-              Hoy
-            </v-btn>
-            <v-btn fab text small @click="prev">
-              <v-icon small>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn fab text small @click="next">
-              <v-icon small>mdi-chevron-right</v-icon>
-            </v-btn>
-            <v-toolbar-title>{{ title }}</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-menu bottom right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  outlined
-                  v-on="on"
-                >
-                  <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right>mdi-menu-down</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item @click="type = 'day'">
-                  <v-list-item-title>Día</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'week'">
-                  <v-list-item-title>Semana</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'month'">
-                  <v-list-item-title>Mes</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = '4day'">
-                  <v-list-item-title>4 Días</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-toolbar>
-        </v-sheet>
-        <v-sheet height="600">
-          <v-calendar
-            locale="es-co"
-            :short-weekdays="false"
-            :short-months="false"
-            :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-            ref="calendar"
-            v-model="focus"
-            color="primary"
-            :events="trabajosCalendario"
-            :event-color="getEventColor"
-            :event-margin-bottom="3"
-            :now="today"
-            :type="type"
-            @click:event="showEvent"
-            @click:more="viewDay"
-            @click:date="viewDay"
-            @change="updateRange"
-          ></v-calendar>
-
-          <v-menu
-            v-model="selectedOpen"
-            :close-on-content-click="false"
-            :activator="selectedElement"
-            offset-x
-          >
-            <v-card
-              color="grey lighten-4"
-              min-width="350px"
-              flat
+    <v-row>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>
+              Trabajos
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Buscar Trabajo"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-title>
+            <v-data-table
+              :headers="headers"
+              :items="trabajosCalendario"
+              :items-per-page="10"
+              :search="search"
+              class="elevation-1"
             >
-              <v-toolbar
-                :color="trabajoSeleccionado.color"
-                dark
-              >
-                <v-toolbar-title v-html="trabajoSeleccionado.name"></v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      icon
-                      color="white"
-                      v-bind="attrs"
-                      v-on="on"
-                      @click="selectedOpen = false"
-                    >
-                      <v-icon>{{ mdiCloseCircleOutline }}</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Cerrar</span>
-                </v-tooltip>
-                <!-- <p class="my-0 mx-2 text-decoration-underline">{{
-                    trabajoSeleccionado.terminado
-                        ? 'Trabajo Listo'
-                        : 'Trabajo no terminado'}}</p> -->
+            <template v-slot:item.terminado="{ item }">
+              <v-chip
+                :color="getColor(item.terminado)"
+                dark>
+                {{ item.terminado === true
+                  ? 'OK'
+                  : 'X'
+                }}
+              </v-chip>
+            </template>
+            </v-data-table>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-sheet height="64">
+            <v-toolbar flat color="blue-grey darken-4">
 
-                <!-- <v-tooltip v-model="mostrarTooltip" bottom>
-                  <template v-slot:activator="{on, attrs}">
-                    <v-btn
-                      v-on="on"
-                      v-bind="attrs"
-                      icon
-                      color="white"
-                      @click.prevent="terminado(trabajoSeleccionado)"
-                    >
-                      <v-icon class="white--text">{{
-                        trabajoSeleccionado.terminado
-                            ? mdiCheckCircleOutline
-                            : mdiCloseCircleOutline}}
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <span>{{
-                    trabajoSeleccionado.terminado
-                        ? 'Trabajo no termiando'
-                        : 'Trabajo termiando'}}
-                  </span>
-                </v-tooltip> -->
-              </v-toolbar>
-              <v-card-text>
-                <span class="black--text" v-html="trabajoSeleccionado.details"></span>
-              </v-card-text>
-              <v-divider color="black"></v-divider>
-              <v-card-actions>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      icon
-                      color="primary"
-                      v-bind="attrs"
-                      v-on="on"
-                      :to="{name: 'ver-trabajo', params: { id: trabajoSeleccionado.id } }"
-                    >
-                      <v-icon>{{ mdiEye }}</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Ver Trabajo</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      :to="{name: 'editar-trabajo', params: { id: trabajoSeleccionado.id } }"
-                      icon
-                      color="success"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      <v-icon>{{ mdiPencil }}</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Editar Trabajo</span>
-                </v-tooltip>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      icon
-                      color="error"
-                      v-bind="attrs"
-                      v-on="on"
-                      @click="confirmarEliminarTrabajo(trabajoSeleccionado)"
-                    >
-                      <v-icon>{{ mdiDelete }}</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Eliminar Trabajo</span>
-                </v-tooltip>
-              </v-card-actions>
-            </v-card>
-          </v-menu>
-        </v-sheet>
-      </v-col>
+              <v-btn outlined class="mr-4" @click="setToday">
+                Hoy
+              </v-btn>
+              <v-btn fab text small @click="prev">
+                <v-icon small>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn fab text small @click="next">
+                <v-icon small>mdi-chevron-right</v-icon>
+              </v-btn>
+              <v-toolbar-title>{{ title }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-menu bottom right>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    outlined
+                    v-on="on"
+                  >
+                    <span>{{ typeToLabel[type] }}</span>
+                    <v-icon right>mdi-menu-down</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item @click="type = 'day'">
+                    <v-list-item-title>Día</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="type = 'week'">
+                    <v-list-item-title>Semana</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="type = 'month'">
+                    <v-list-item-title>Mes</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="type = '4day'">
+                    <v-list-item-title>4 Días</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-toolbar>
+          </v-sheet>
+          <v-sheet height="600">
+            <v-calendar
+              locale="es-co"
+              :short-weekdays="false"
+              :short-months="false"
+              :weekdays="[1, 2, 3, 4, 5, 6, 0]"
+              ref="calendar"
+              v-model="focus"
+              color="primary"
+              :events="trabajosCalendario"
+              :event-color="getEventColor"
+              :event-margin-bottom="3"
+              :now="today"
+              :type="type"
+              @click:event="showEvent"
+              @click:more="viewDay"
+              @click:date="viewDay"
+              @change="updateRange"
+            ></v-calendar>
+
+            <v-menu
+              v-model="selectedOpen"
+              :close-on-content-click="false"
+              :activator="selectedElement"
+              offset-x
+            >
+              <v-card
+                color="grey lighten-4"
+                min-width="350px"
+                flat
+              >
+                <v-toolbar
+                  :color="trabajoSeleccionado.color"
+                  dark
+                >
+                  <v-toolbar-title v-html="trabajoSeleccionado.name"></v-toolbar-title>
+                  <v-spacer></v-spacer>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        color="white"
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="selectedOpen = false"
+                      >
+                        <v-icon>{{ mdiCloseCircleOutline }}</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Cerrar</span>
+                  </v-tooltip>
+                </v-toolbar>
+                <v-card-actions
+                  :class="trabajoSeleccionado.terminado === false ? 'black--text' : 'siTerminado' "
+                >
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        color="primary"
+                        v-bind="attrs"
+                        v-on="on"
+                        :to="{name: 'ver-trabajo', params: { id: trabajoSeleccionado.id } }"
+                      >
+                        <v-icon>{{ mdiEye }}</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Ver Trabajo</span>
+                  </v-tooltip>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        :to="{name: 'editar-trabajo', params: { id: trabajoSeleccionado.id } }"
+                        icon
+                        color="success"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <v-icon>{{ mdiPencil }}</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Editar Trabajo</span>
+                  </v-tooltip>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        color="error"
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="confirmarEliminarTrabajo(trabajoSeleccionado)"
+                      >
+                        <v-icon>{{ mdiDelete }}</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Eliminar Trabajo</span>
+                  </v-tooltip>
+                  <v-spacer></v-spacer>
+                  <div>
+                    {{trabajoSeleccionado.terminado === false
+                          ? 'Trabajo sin Terminar'
+                          : 'Trabajo Terminado'}}
+                  </div>
+                </v-card-actions>
+              </v-card>
+            </v-menu>
+          </v-sheet>
+        </v-col>
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -199,7 +209,6 @@ import {
   mdiCancel,
 } from '@mdi/js';
 import Swal from 'sweetalert2';
-import { db } from '@/firebase';
 
 export default {
   name: 'Admin',
@@ -230,6 +239,15 @@ export default {
       details: null,
       color: '#1976D2',
       currentlyEditing: null,
+      headers: [
+        { text: 'Ciente', sortable: false, value: 'name' },
+        { text: 'Asesor', value: 'asesor' },
+        { text: 'Detalles', sortable: false, value: 'details' },
+        { text: 'Fecha Inicio', value: 'start' },
+        { text: 'Fecha de Entrega', value: 'end' },
+        { text: 'Estado', value: 'terminado' },
+      ],
+      search: '',
     };
   },
   metaInfo: {
@@ -279,6 +297,11 @@ export default {
   },
   methods: {
     ...mapActions(['traerTrabajosCalendario', 'eliminarTrabajoCalendario']),
+    getColor(terminado) {
+      if (terminado === true) return 'green';
+      if (terminado === false) return 'red';
+      return terminado;
+    },
     viewDay({ date }) {
       this.focus = date;
       this.type = 'day';
@@ -343,32 +366,18 @@ export default {
         }
       });
     },
-    async terminado(trabajo) {
-      if (trabajo.terminado) {
-        await db.collection('trabajos').doc(trabajo.id).update({
-          terminado: false,
-        });
-        Swal.fire(
-          '¡Sin terminar!',
-          'El trabajo aún no está terminado.',
-          'error',
-        );
-        this.selectedOpen = false;
-      } else {
-        await db.collection('trabajos').doc(trabajo.id).update({
-          terminado: true,
-        });
-        Swal.fire(
-          '¡Terminado!',
-          'El trabajo ha sido terminado.',
-          'success',
-        );
-        this.selectedOpen = false;
-      }
-    },
   },
   created() {
     this.$store.commit('setLayout', 'adminLayout');
   },
 };
 </script>
+
+<style>
+  .siTerminado {
+    background: darkgreen;
+  }
+  .siTerminadoBoton {
+    color: darkgreen !important;
+  }
+</style>
