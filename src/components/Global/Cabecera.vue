5dda4b6c6369b708cc8215d42fd53d23760f37ee
Cristhian Daza <cristian.d.b@hotmail.com>
2020-11-06 16:23:35 -0500

    Mostrando boton para cambiar de tema en la cabecera

--- src/components/Global/Cabecera.vue
+++ src/components/Global/Cabecera.vue
@@ -2,6 +2,29 @@
   <div>
     <v-container>
       <v-layout>
+          <div class="absolute">
+            <v-tooltip v-model="mostrarTooltip" bottom>
+              <template v-slot:activator="{on, attrs}">
+                <v-btn
+                  v-on="on"
+                  v-bind="attrs"
+                  icon
+                  @click="cambiarTema"
+                >
+                  <v-icon >{{
+                    $vuetify.theme.dark
+                        ? mdiWhiteBalanceSunny
+                        : mdiMoonWaningCrescent}}
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>{{
+                $vuetify.theme.dark
+                    ? 'Activar modo Claro'
+                    : 'Activar modo Oscuro'}}
+              </span>
+            </v-tooltip>
+          </div>
         <v-row class="d-flex">
           <v-col cols="12" sm="6" :class="this.$vuetify.breakpoint.xs ? 'order-2' : ''">
             <Logo width="200"/>
@@ -43,19 +66,62 @@
         </v-row>
       </v-layout>
     </v-container>
+    <v-snackbar
+      v-model="snackbar"
+      class="mt-5"
+      shaped
+      top
+      text
+      :color="
+        $vuetify.theme.dark
+            ? 'primary'
+            : 'grey darken-4'
+      "
+    >
+      {{ textSnackbar }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          v-bind="attrs"
+          text
+          @click="snackbar = false"
+        >
+          Cerrar
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
 import Logo from '@/components/Global/Logo.vue';
-import { mdiWhatsapp } from '@mdi/js';
+import { mdiWhatsapp, mdiMoonWaningCrescent, mdiWhiteBalanceSunny } from '@mdi/js';
 
 export default {
   name: 'Cabecera',
   data() {
     return {
       mdiWhatsapp,
+      mdiMoonWaningCrescent,
+      mdiWhiteBalanceSunny,
+      snackbar: false,
+      textSnackbar: '',
+      mostrarTooltip: false,
     };
+  },
+  methods: {
+    cambiarTema() {
+      if (this.$vuetify.theme.dark) {
+        this.textSnackbar = 'Modo Claro Activado';
+        this.snackbar = true;
+        this.$vuetify.theme.dark = false;
+        localStorage.setItem('darkTheme', this.$vuetify.theme.dark.toString());
+      } else {
+        this.textSnackbar = 'Modo Oscuro Activado';
+        this.snackbar = true;
+        this.$vuetify.theme.dark = true;
+        localStorage.setItem('darkTheme', this.$vuetify.theme.dark.toString());
+      }
+    },
   },
   components: {
     Logo,
