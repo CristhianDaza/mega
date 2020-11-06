931dae277b2fe7fdd7b3a1ba3709abbdda2246c5
Cristhian Daza <cristian.d.b@hotmail.com>
2020-11-06 13:23:39 -0500

    Agregando archivos de ruta menuprincipal

---
+++ src/views/admin/menuPrincipal/Index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div :class="this.$vuetify.breakpoint.xs ? '' : 'container'">
+    <v-btn
+      color="success"
+      large outlined
+      to="/admin/menu-principal/agregar-menu">
+      Agregar Menú
+    </v-btn>
+    <v-divider class="my-3"></v-divider>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Etiqueta</th>
+            <th class="text-right">Acción</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- <tr v-for="producto in titulos" :key="producto.uid">
+            <td>{{producto.titulo}}</td>
+            <td>{{producto.etiqueta}}</td>
+            <td class="text-right">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    color="red"
+                    v-bind="attrs"
+                    v-on="on"
+                    class="ml-2"
+                    @click="confirmarEliminarProduto(producto.id)"
+                  >
+                    <v-icon>{{mdiDelete}}</v-icon>
+                  </v-btn>
+                </template>
+                <span>Eliminar</span>
+              </v-tooltip>
+            </td>
+          </tr> -->
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+// import { mapActions, mapState } from 'vuex';
+import Swal from 'sweetalert2';
+import { mdiDelete } from '@mdi/js';
+
+export default {
+  name: 'productos-inicio',
+  data() {
+    return {
+      mdiDelete,
+    };
+  },
+  metaInfo: {
+    title: 'Productos Inicio',
+    titleTemplate: '%s | Megapromocionales LTDA',
+    meta: [
+      { charset: 'utf8' },
+      { name: 'robots', content: 'noindex' },
+    ],
+  },
+  methods: {
+    // ...mapActions(['traerProducto', 'eliminarProducto']),
+    confirmarEliminarProduto(id) {
+      Swal.fire({
+        title: '¿Estas segur@?',
+        text: '¡No se podrá revertir!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, ¡eliminarlo!',
+      }).then((result) => {
+        if (result.value) {
+          this.eliminarProducto(id);
+          Swal.fire(
+            '¡Eliminada!',
+            'El producto ha sido eliminado.',
+            'success',
+          );
+        }
+      });
+    },
+  },
+  computed: {
+    // ...mapState(['titulos']),
+  },
+  mounted() {
+    this.traerProducto();
+  },
+  created() {
+    this.$store.commit('setLayout', 'adminLayout');
+  },
+};
+</script>
