bdd1ecf97dcd596d9639c895c6973a8332f4c049
Cristhian Daza <cristian.d.b@hotmail.com>
2020-11-07 12:09:26 -0500

    Agregando colores base

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import es from 'vuetify/es5/locale/es';
-// import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
@@ -16,10 +15,14 @@ export default new Vuetify({
     themes: {
       light: {
         primary: '#0077bd',
-        background: '#eeeeee',
+        texto: '#252525',
+        basebackground: '#ffffff',
+        background: '#f2f5f8',
       },
       dark: {
         secondary: '#05090c',
+        texto: '#edf0f2',
+        basebackground: '#05090c',
         background: '#111b27',
       },
     },
