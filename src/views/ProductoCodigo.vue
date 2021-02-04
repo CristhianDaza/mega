5c4788fe66d7744155115395081e15118af12f82
Cristhian Daza <cristian.d.b@hotmail.com>
2021-02-04 17:43:35 -0500

    Quitando autoplay al video

--- src/views/ProductoCodigo.vue
+++ src/views/ProductoCodigo.vue
@@ -352,12 +352,15 @@
             <v-responsive :aspect-ratio="16/9">
               <div class="player-container">
                 <vue-core-video-player
+                  :autoplay="false"
                   muted
                   loop
                   preload="metadata"
                   :title="`${productoCodigo[0].familia} ${productoCodigo[0].descripcion_comercial}`"
                   logo="https://firebasestorage.googleapis.com/v0/b/megapromocionales2020.appspot.com/o/opt_logo.webp?alt=media&amp;token=2e13ac36-784c-463a-a094-fa665516fffe&quot"
-                  :src="productoCodigo[0].videos[0].video">
+                  :src="productoCodigo[0].videos[0].video"
+                  :cover="productoCodigo[0].imagenes[0].imagen.file_md"
+                  >
                 </vue-core-video-player>
               </div>
             </v-responsive>
