62b8e7c5ad3196df1af5eec4c62176ef886e9c20
Cristhian Daza <cristian.d.b@hotmail.com>
2021-03-24 16:59:32 -0500

    Creando componente para los productos sugeridos

--- src/views/ProductoCodigo.vue
+++ src/views/ProductoCodigo.vue
@@ -74,94 +74,9 @@
           </div>
         </v-col>
         <v-col cols="12" sm="6">
-          <h1 class="primary--text text-center mt-2">
-            Productos Sugeridos
-          </h1>
-
-          <v-divider class="mx-10"></v-divider>
-          <h3 v-if="this.textoSugerido !== ''" class="text-center">
-            {{this.textoSugerido}}
-          </h3>
-          <v-row class="mt-3" v-if="productoSugerencia.length > 0">
-            <v-col cols="6" v-for="producto in productoSugerencia" :key="producto.familia">
-              <v-card
-                :style="{background: $vuetify.theme.themes[theme].basebackground}"
-                outlined
-                class="mx-auto tarjetaProducto">
-                <v-card-text class="pa-0">
-                  <router-link
-                    append
-                    :to="{path: `/producto/${producto.producto.familia}`}"
-                  >
-                    <v-img
-                      :src="producto.imagen.imagen.file_md"
-                      width="100%"
-                      :alt="producto.descripcion_comercial"
-                      class="imagenProducto"
-                    >
-                      <template v-slot:placeholder>
-                        <v-row
-                          class="fill-height ma-0"
-                          align="center"
-                          justify="center"
-                        >
-                          <Loader />
-                        </v-row>
-                      </template>
-                    </v-img>
-                  </router-link>
-                </v-card-text>
-                    <v-card-title
-                  class="text-center text-subtitle-2"
-                  :style="{color: $vuetify.theme.themes[theme].basetexto}"
-                >
-                  {{ producto.producto.descripcion_comercial }}
-                </v-card-title>
-                <v-card-subtitle
-                  :style="{color: $vuetify.theme.themes[theme].basetexto}"
-                >
-                  {{ producto.producto.familia }}
-                </v-card-subtitle>
-                <v-card-text class="text-center">
-                  <div class="red--text" v-if="producto.inventario < 10">Agotado en {{producto.color_nombre}}</div>
-                  <div v-else :style="{color: $vuetify.theme.themes[theme].basetexto}">Inventario: {{producto.inventario}} color {{producto.color_nombre}}.</div>
-                </v-card-text>
-                <div class="text-center">
-                  <template v-if="productoCodigo[0].etiquetas.length > 0">
-                    <div
-                      v-for="etiqueta in productoCodigo[0].etiquetas"
-                      :key="etiqueta.id"
-                    >
-                      <img
-                        width="80px"
-                        :src="etiqueta.imagen.file_sm"
-                        :alt="etiqueta.nombre"
-                        >
-                    </div>
-                  </template>
-                </div>
-                <v-divider class="mx-5"></v-divider>
-                <v-card-actions>
-                <v-btn
-                  :to="{path: `/producto/${producto.producto.familia}`}"
-                  outlined block
-                  color="primary"
-                  text
-                  append
-                >
-                    Ver Producto
-                </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-          <div v-else class="mx-auto">
-            <v-container class="fill-height mt-16 mx-auto">
-              <v-row align="center" justify="center">
-                <Loader />
-              </v-row>
-            </v-container>
-          </div>
+          <SugeridoProducto
+            :sugerencia="productoSugerencia"
+          />
         </v-col>
       </v-row>
 
@@ -227,6 +142,7 @@ import InfoProducto from '@/components/Producto/InfoProducto.vue';
 import ImagenesProducto from '@/components/Producto/ImagenesProducto.vue';
 import ExistenciasProducto from '@/components/Producto/ExistenciasProducto.vue';
 import VideoProducto from '@/components/Producto/VideoProducto.vue';
+import SugeridoProducto from '@/components/Producto/SugeridoProducto.vue';
 import axios from 'axios';
 import {
   mdiCloseCircleOutline,
@@ -237,7 +153,6 @@ export default {
   data() {
     return {
       loader: null,
-      textoSugerido: '',
       textoInfo: '',
       productoCodigo: [],
       productoSugerencia: [],
@@ -420,6 +335,7 @@ export default {
     ImagenesProducto,
     ExistenciasProducto,
     VideoProducto,
+    SugeridoProducto,
   },
 };
 </script>
@@ -468,12 +384,6 @@ export default {
 
 .links ul {
   padding: 0;
-}
-.tarjetaProducto {
-  transition: all .4s;
-}
-.tarjetaProducto:hover {
-  transform: translateY(-15px);
 }
 .custom-loader {
   animation: loader 1s infinite;
