dc3cb87bf3f08ba8cdbcba958a41c0746bedd74e
Cristhian Daza <cristian.d.b@hotmail.com>
2021-05-29 14:51:41 -0500

    Agregando fondo al video

--- src/components/Index/VideoPromocion.vue
+++ src/components/Index/VideoPromocion.vue
@@ -1,47 +1,60 @@
 <template>
-  <v-card
-    :style="{background: $vuetify.theme.themes[theme].background}"
-  >
-    <v-responsive
-      v-for="video in videos"
-      :key="video.id"
-      :class="video.completo.value ? '' : 'container'"
+  <div
+  class="fondoVideo"
+  :style="{ backgroundImage: `url(${VideoProm})` }">
+    <v-card
+      class="fondoCard"
     >
-      <router-link :to="video.urlProducto">
-        <div class="player-container">
-          <vue-core-video-player
-            loop
-            :muted="true"
-            :controls="false"
-            preload="metadata"
-            width="100%"
-            logo="https://firebasestorage.googleapis.com/v0/b/megapromocionales2020.appspot.com/o/opt_logo.webp?alt=media&amp;token=2e13ac36-784c-463a-a094-fa665516fffe&quot"
-            :src="video.linkVideo"
-            type="video/mp4"
-            >
-          </vue-core-video-player>
-        </div>
-      </router-link>
-    </v-responsive>
-  </v-card>
+      <v-responsive
+        v-for="video in videos"
+        :key="video.id"
+        :class="video.completo.value ? '' : 'container'"
+      >
+        <router-link :to="video.urlProducto">
+          <div class="player-container">
+            <vue-core-video-player
+              loop
+              :muted="true"
+              :controls="false"
+              preload="metadata"
+              width="100%"
+              logo="https://firebasestorage.googleapis.com/v0/b/megapromocionales2020.appspot.com/o/opt_logo.webp?alt=media&amp;token=2e13ac36-784c-463a-a094-fa665516fffe&quot"
+              :src="video.linkVideo"
+              type="video/mp4"
+              >
+            </vue-core-video-player>
+          </div>
+        </router-link>
+      </v-responsive>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import VideoProm from '@/assets/videoProm.svg';
 
 export default {
   name: 'videoComponent',
+  data() {
+    return {
+      VideoProm,
+    };
+  },
   methods: {
     ...mapActions(['traerVideos']),
   },
   computed: {
     ...mapState(['videos']),
-    theme() {
-      return (this.$vuetify.theme.dark) ? 'dark' : 'light';
-    },
   },
   mounted() {
     this.traerVideos();
   },
 };
 </script>
+
+<style>
+.fondoVideo {
+  background-size: 100%;
+}
+</style>
