25cc9b1be1d754dcd33c180bda31658143be3fc8
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-09 13:31:07 -0500

    Actualizando boton cambiar tema

--- src/components/Global/BotonTema.vue
+++ src/components/Global/BotonTema.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="botonTema">
-    <v-switch
-      v-model="botonTema"
-      @change="cambiarTema"
-      inset
-      :class="$vuetify.theme.dark ? 'icon-dark' : 'icon-light'"
+    <button
+      class="switch"
+      :class="$vuetify.theme.dark ? 'active' : ''"
+      id="switch"
+      @click="cambiarTema"
     >
-    </v-switch>
+      <span>
+        <v-icon
+         color="black"
+        >
+          {{ mdiWhiteBalanceSunny }}
+        </v-icon>
+      </span>
+      <span>
+        <v-icon
+          color="white"
+        >
+          {{ mdiMoonWaningCrescent }}
+        </v-icon>
+      </span>
+    </button>
   </div>
 </template>
 
@@ -19,7 +33,6 @@ export default {
       mostrarTooltip: false,
       mdiWhiteBalanceSunny,
       mdiMoonWaningCrescent,
-      botonTema: false,
     };
   },
   methods: {
@@ -33,22 +46,62 @@ export default {
       }
     },
   },
-  mounted() {
-    const theme = localStorage.getItem('darkTheme');
-    if (theme) {
-      if (theme === 'true') {
-        this.botonTema = true;
-      } else {
-        this.botonTema = false;
-        this.$vuetify.theme.dark = false;
-      }
-    } else {
-      this.botonTema = false;
-      this.$vuetify.theme.dark = false;
-    }
-  },
 };
 </script>
 
 <style>
+.botonTema {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.switch {
+  background: #111b27;
+  border-radius: 1000px;
+  border: none;
+  position: relative;
+  cursor: pointer;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  outline: none;
+  transition: all .2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.switch::after {
+  content: '';
+  display: block;
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  background: #f1f1f1;
+  top: 0;
+  left: 0;
+  right: unset;
+  border-radius: 100px;
+  -webkit-transition: 0.3s ease all;
+  transition: 0.3s ease all;
+  -webkit-box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.2);
+}
+
+.switch.active {
+  background: #f2f5f8;
+  color: #000;
+}
+
+.switch.active::after {
+  right: 0;
+  left: unset;
+}
+
+.switch span {
+  width: 30px;
+  height: 30px;
+  line-height: 30px;
+  display: block;
+  background: none;
+  color: #fff;
+}
 </style>
