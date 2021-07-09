5ff449ff04adfad99b79eadb13de65d0828e1c72
Cristhian Daza <cristian.d.b@hotmail.com>
2021-07-09 14:48:34 -0500

    Agregando plugin reproductor

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueMeta from 'vue-meta';
 import { auth } from '@/firebase';
 import VueCarousel from 'vue-carousel';
 import flipbook from 'flipbook-vue';
-import VueCoreVideoPlayer from 'vue-core-video-player';
 import moment from 'moment';
 import router from './router';
 // eslint-disable-next-line import/order
@@ -12,51 +11,16 @@ import App from './App.vue';
 // eslint-disable-next-line import/no-cycle
 import store from './store';
 import vuetify from './plugins/vuetify';
+import './plugins/reproductor';
 import '@babel/polyfill';
 import './assets/css/globalstyle.css';
 
 Vue.config.productionTip = false;
 Vue.prototype.moment = moment;
 
-const es = {
-  dashboard: {
-    btn: {
-      play: 'Reproducir',
-      pause: 'Pausa',
-      fullscreen: 'Pantalla completa',
-      exitFullscreen: 'Salir de pantalla completa',
-      mute: 'Silenciar',
-      unmute: 'Activar sonido',
-      back: 'Atrás',
-      pip: 'Reproductor en miniatura',
-    },
-    settings: {
-      autoplay: 'Autoplay',
-      loop: 'Bucle',
-      speed: 'Velocidad',
-      resolution: 'Resolución',
-    },
-  },
-  layers: {
-    error: {
-      title: '(O_O)?  Error!',
-      2404: 'Fuente de video no encontrada',
-      2502: 'Error de red',
-      2503: 'El video no se puede decodificar',
-      2504: '¡No se puede reproducir el video!',
-    },
-    loading: {
-      msg: 'Cargando...',
-    },
-  },
-};
-
 Vue.use(VueMeta);
 Vue.use(VueCarousel);
 Vue.use(flipbook);
-Vue.use(VueCoreVideoPlayer, {
-  lang: es,
-});
 Vue.use(VueAnalytics, {
   id: 'UA-126390971-1',
   router,
